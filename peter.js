e53a6aed0de2cc44031eb1924f9872d73ec2b1af
PeterA1 <peteralderson2@gmail.com>
2013-11-22 12:25:58 +0000

    Update peter.js

--- peter.js
+++ peter.js
@@ -18,7 +18,13 @@ var operations = {
       
       sys.puts(sys.inspect(parts));
       
-      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      
+      res.write("<h1>Hello World!</h1>");
+      
+      res.write(result);
+      
+      res.end("<h2>This is the end!</h2>");
       
       res.end(result);
       

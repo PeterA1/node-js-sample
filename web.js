861974614fd52c5106c654831c934cf6af24af09
PeterA1 <peteralderson2@gmail.com>
2013-11-22 11:05:41 +0000

    Update web.js
    Just more changes

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Hello Peter!! Hello with commit');
+  response.send('Hello World! Hello Peter!! Now we are using Git for Code Maint');
 });
 
 var port = process.env.PORT || 5000;

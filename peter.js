39264f449a2f29232c0884fd1c2a09b16c0ea95a
PeterA1 <peteralderson2@gmail.com>
2013-11-22 12:39:19 +0000

    Update peter.js

--- peter.js
+++ peter.js
@@ -18,7 +18,7 @@ var operations = {
       
       sys.puts(sys.inspect(parts));
       
-      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.writeHead(200, {'Content-Type': 'text/html'});
       
       res.end("" + result);
       

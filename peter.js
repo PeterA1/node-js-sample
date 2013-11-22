2de8ab1fd34a1508c86ca4646f2786a4eca7066a
PeterA1 <peteralderson2@gmail.com>
2013-11-22 12:57:41 +0000

    Update peter.js

--- peter.js
+++ peter.js
@@ -1,6 +1,9 @@
 var sys = require('sys'),
     http = require('http');
     
+var funInput = ["add","sub","mul","div"];
+var funDescription = [" Added to ", " to be reduced by "," to be multiplied by "," to be divided by "]
+    
 var operations = {
     add: function(a,b) { return a + b },
     sub: function(a,b) { return a - b },
@@ -13,6 +16,7 @@ var operations = {
         op = operations[parts[1]],
         a = parseInt(parts[2], 10),
         b = parseInt(parts[3], 10);
+        funDisplay = funDescription[funInput[parts[1]]];
 
       var result = "" + op(a,b);
       
@@ -22,7 +26,7 @@ var operations = {
       
       res.write("<h1>The Worlds simplest Web Service in Node.js</h1>");
       
-      res.write("<h2>You have asked for " + parts[2] + " " + parts[1] + " " + parts[3] + "</h2>")
+      res.write("<h2>You have asked for " + parts[2] + funDisplay + parts[3] + "</h2>")
       
       res.end(result);
       

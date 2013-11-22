eda17d921fa028a2522c7a264b74374d7edfb6b2
PeterA1 <peteralderson2@gmail.com>
2013-11-22 12:01:50 +0000

    Update peter.js

--- peter.js
+++ peter.js
@@ -2,6 +2,7 @@ var sys = require('sys'),
     http = require('http');
     
     http.createServer(function(req, res) {
+      sys.puts(sys.inspect(req));
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write("<h1>Hello World!<h1>");
       res.end("<h2>This is the end!!<h2>");

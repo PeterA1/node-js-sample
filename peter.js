67848a6dcd6efa5c9eaf7e6cac902665c5950ea0
PeterA1 <peteralderson2@gmail.com>
2013-11-22 11:50:40 +0000

    Create peter.js
    Hello Worlds Mk1

---
+++ peter.js
@@ -0,0 +1,10 @@
+var sys = require('sys'),
+    http = require('http');
+    
+    http.createServer(function(req, res) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write("<h1>Hello World!<h1>");
+      res.end("<h2>This is the end!!<h2>");
+    }).listen(5000, "54.201.70.38");
+    
+sys.puts("Server running at http://54.201.70.38:5000/");

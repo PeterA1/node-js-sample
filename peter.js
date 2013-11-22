b784e3f374f42e7ba617578879cd7f3e9333fe0d
PeterA1 <peteralderson2@gmail.com>
2013-11-22 12:22:42 +0000

    Update peter.js

--- peter.js
+++ peter.js
@@ -1,15 +1,27 @@
 var sys = require('sys'),
     http = require('http');
     
+var operations = {
+    add: function(a,b) { return a + b },
+    sub: function(a,b) { return a - b },
+    mul: function(a,b) { return a * b },
+    div: function(a,b) { return a / b }
+};
     http.createServer(function(req, res) {
       
-      var parts = req.url.split("/");
+      var parts = req.url.split("/"),
+        op = operations(parts[1]),
+        a = parsetInt(parts[2], 10),
+        b = parstInt(parts[3], 10);
+
+      var result = op(a,b);
       
       sys.puts(sys.inspect(parts));
       
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("<h1>Hello World!<h1>");
-      res.end("<h2>This is the end!!<h2>");
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      
+      res.end(result);
+      
     }).listen(5000);
     
 console.log("Server running at port 5000");

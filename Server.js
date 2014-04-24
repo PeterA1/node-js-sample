5f76a8e6b815e29f916d1ec531957e93d1cf6d7a
Peter <peteralderson2@gmail.com>
2014-04-24 09:42:30 +0100

    Added Timer

--- Server.js
+++ Server.js
@@ -1,3 +1,7 @@
+Days  = [ "Sunday","Monday", "Tuseday", "Wednesday","Thursday","Friday","Saturday","Sunday","Monday", "Tuseday", "Wednesday","Thursday","Friday","Saturday "]; 
+Month = [ "January", "February","March","April","May","June","July","August","September","October","November","December","January", "February","March","April","May","June","July","August","September","October","November","December" ];
+Mth   = [ [ 1, 21, 31 ],[ 2, 22 ],[ 3, 23 ] ]; // st,nd,rd (others are th).
+
 var WebSocketServer = require('ws').Server
 
 var Traffic = new Array();
@@ -8,7 +12,7 @@ Counter = 1000;
 
 wss = new WebSocketServer({port: 5000});
 
-wss.on('connection', function(ws) { ws.id = "myUID"+Counter;  ws.send(ws.id); Counter = Counter + 1; Traffic.push(ws); console.log("Opened to : ",ws.id);
+wss.on('connection', function(ws) { ws.id = "myUID"+Counter;  ws.send(ws.id); Counter = Counter + 1; Traffic.push(ws); Now = new Date(); console.log("Opened to : ",ws.id, GetDate(Now));
                                     ws.on('message', function(message) { MessageRecieved(ws,message); } ); 
                                     ws.addEventListener('close', function(code)    { wsRemove(code.target.id); return; },true );
                                     ws.addEventListener('error', function(error)   { console.log("Error  :",error); return; },true );
@@ -34,13 +38,9 @@ function SendMessage(ws,message) {
 function wsRemove(faultyconnection) {
   
     for (k=0;k<Traffic.length;k++) {
-      
-
-      
-      if (Traffic[k].id == faultyconnection ) { Traffic[k].id = ""; console.log("Removed :" + faultyconnection); return; };
-
+      if (Traffic[k].id == faultyconnection ) { Traffic[k].id = ""; Now = new Date(); console.log("Removed :" + faultyconnection, GetDate(Now) ); return; }; 
     };
-    
+    Now = new Date(); console.log("Could not find :" + faultyconnection, GetDate(Now)); 
 };
     
 function GetDate(date) {
@@ -60,10 +60,11 @@ fDate.setTime(date);
     Minutes = fDate.getMinutes();
     if (Hours < 12) { AMPM = "am"; }; if (Hours > 11) { AMPM = "pm"; if (Hours > 12) { Hours = Hours - 12; }; };
     Minutes = Minutes + AMPM; if (Minutes.length < 4) { Minutes = "0" + Minutes };
-    rDate += ", "+ Hours + "." + Minutes;
+    rDate += ", "+ Hours + "." + Minutes +" (+"+fDate.getMilliseconds()+"ms)";
 
 return rDate;
 
 };
     
-  
+   
+    
